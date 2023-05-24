29d5a02e631e84edea8e5211997bba90af59c445
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-05-23 21:25:10 -0400

    complete

--- index.js
+++ index.js
@@ -21,3 +21,80 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(array){
+
+return {
+    firstName: array[0],
+    familyName: array[1],
+    title:array[2],
+    payPerHour: array[3],
+    timeInEvents:[],
+    timeOutEvents:[]
+
+}
+}
+
+function createEmployeeRecords(array){
+  const newArray =  array.map(x => createEmployeeRecord(x))
+return newArray
+}
+
+function createTimeInEvent(time){
+
+
+    let timeComponent = time.split(" ")
+    
+    let event = {
+        type: "TimeIn",
+        hour: parseInt(timeComponent[1]),
+        date: timeComponent[0]
+
+    }
+    
+    this.timeInEvents.push(event)
+
+    return this 
+}
+
+function createTimeOutEvent(time){
+
+
+    let timeComponent = time.split(" ")
+    
+    let event = {
+        type: "TimeOut",
+        hour: parseInt(timeComponent[1]),
+        date: timeComponent[0]
+
+    }
+   
+    this.timeOutEvents.push(event)
+
+    return this 
+}
+
+function hoursWorkedOnDate(date){
+ let dateIn = this.timeInEvents.find(x =>{ 
+  return  x.date === date 
+ })
+
+
+let dateOut = this.timeOutEvents.find(x =>{ 
+    return  x.date === date 
+   })
+  let hours = (dateOut.hour - dateIn.hour)
+
+  return hours/100
+}
+ function wagesEarnedOnDate(date){
+   return  this.payPerHour * hoursWorkedOnDate.call(this, date)
+}
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find((employeeRecord)=> employeeRecord.firstName == firstName)
+    
+}
+
+function calculatePayroll(array){
+   const initialValue = 0
+return array.reduce((accum, initialValue) => accum + allWagesFor.call(initialValue),initialValue)
+}
